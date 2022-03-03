17794995c09ffff7182d750dfb2c937e75570997
yogesh1232 <yogeshkusakiya77@gmail.com>
2022-03-03 18:49:09 +0530

    [Yogesh] Adding Create data table

--- ProductReviewManagement/ProductReviewManagement/DisplayProducts.cs
+++ ProductReviewManagement/ProductReviewManagement/DisplayProducts.cs
@@ -9,33 +9,41 @@ namespace ProductReviewManagement
 {
     public class DisplayProducts
     {
+        public static DataTable table = new DataTable();
+        /// Creates the data table.
         public static void CreateDataTable()
         {
-            //create obj of DataTable Class.
-            DataTable table = new DataTable();
             //Add Columns in Table
-            table.Columns.Add("Id");
-            table.Columns.Add("ProductName");
+            table.Columns.Add("ProductId");
+            table.Columns.Add("UserId");
+            table.Columns.Add("Rating");
+            table.Columns.Add("Review");
+            table.Columns.Add("IsLike");
             //Add Rows in Table
-            table.Rows.Add(1, "Laptop");
-            table.Rows.Add(2, "Desktop");
-            table.Rows.Add(3, "Smartphone");
-            GetDataFromDataTable(table);
+            table.Rows.Add(1, 1, 9, "bad", "false");
+            table.Rows.Add(2, 7, 6, "good", "true");
+            table.Rows.Add(4, 9, 5, "good", "false");
+            table.Rows.Add(7, 4, 4, "nice", "true");
+            table.Rows.Add(5, 7, 1, "bad", "false");
+            table.Rows.Add(7, 12, 5, "nice", "true");
+            table.Rows.Add(3, 5, 8, "good", "false");
+            table.Rows.Add(3, 14, 4, "bad", "false");
+            table.Rows.Add(9, 10, 1, "bad", "true");
+            table.Rows.Add(9, 11, 3, "bad", "false");
+            table.Rows.Add(3, 10, 10, "good", "false");
+            table.Rows.Add(2, 9, 9, "good", "true");
         }
-        /// <summary>
-        /// Gets the data from data table
-        /// </summary>
-        /// <param name="table">The table.</param>
-        public static void GetDataFromDataTable(DataTable table)
+        /// Gets the data from data table.
+        public static void GetDataFromDataTable()
         {
             try
             {
                 //Query syntax of LINQ
-                var ProductNames = (from product in table.AsEnumerable() select product.Field<string>("ProductName"));
-                //iterate loop to get ProductName
-                foreach (string p in ProductNames)
+                var ProductNames = from product in table.AsEnumerable() select product;
+                //iterate loop to get ProductName.
+                foreach (var data in ProductNames)
                 {
-                    Console.WriteLine("Product Name:" + p);
+                    Console.WriteLine("ProductID: " + data.Field<string>("ProductId") + ", UserID: " + data.Field<string>("UserId") + ", Ratings: " + data.Field<string>("Rating") + " , Review: " + data.Field<string>("Review") + " , IsLike: " + data.Field<string>("IsLike"));
                 }
             }
             catch (Exception ex)
